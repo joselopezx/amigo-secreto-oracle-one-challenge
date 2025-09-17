28e52ab990e261959eee51a8f16823c2c9646ba9
Miguel <jose.garcia2044@alumnos.udg.mx>
2025-09-16 18:38:39 -0600

    Actualización listar nombres guardados

--- app.js
+++ app.js
@@ -2,14 +2,29 @@
 let nombres = []
 
 function agregarAmigo(){
+   //Diferentes formas para tomar el elemto del imput
    // let entrada = document.getElementById("amigo");
     let entrada = document.querySelector("#amigo")
     let nombre = entrada.value.trim();
-    alert(nombre)
     if(nombre == ""){
         alert("Ingrese un nombre para agregarlo");
     }else{
         nombres.push(nombre);
         document.getElementById("amigo").value = "";
+        listarNombres();
     }
 }
+
+function listarNombres(){
+let lista = document.getElementById("listaAmigos");
+lista.innerHTML="";
+let tamanioLista = nombres.length;
+let i=0;
+for(i=0; i<tamanioLista; i++){
+    const li = document.createElement("li");
+    li.textContent = nombres[i];
+    lista.appendChild(li);
+}
+
+}
+
